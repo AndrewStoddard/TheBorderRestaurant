ac8cdc9c41f8932ac1acca105dd65d4113beebea
AndrewStoddard <acs8182000@gmail.com>
2021-04-27 18:40:14 -0400

    Add project files.

---
+++ TheBorderRestaurant/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBorderRestaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Methods
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
