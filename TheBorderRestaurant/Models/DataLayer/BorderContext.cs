b716af14f8b1c8aa717d7eafdfd29a18b76dabec
AndrewStoddard <acs8182000@gmail.com>
2021-04-27 20:34:31 -0400

    Added a border context

---
+++ TheBorderRestaurant/Models/DataLayer/BorderContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TheBorderRestaurant.Models.DomainModels;
+
+namespace TheBorderRestaurant.Models.DataLayer
+{
+    public class BorderContext : IdentityDbContext<User>
+    {
+        #region Properties
+
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<FoodOrder> FoodOrders { get; set; }
+        public DbSet<FoodOrderItem> FoorOrderItems { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BorderContext(DbContextOptions<BorderContext> options) : base(options)
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public static async Task CreateAdminUser(IServiceProvider serviceProvider)
+        {
+            var userManager =
+                serviceProvider.GetRequiredService<UserManager<User>>();
+            var roleManager =
+                serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var username = "admin";
+            var password = "password";
+            var roleName = "Admin";
+
+            if (await roleManager.FindByNameAsync(roleName) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+
+            if (await userManager.FindByNameAsync(username) == null)
+            {
+                var user = new User {UserName = username};
+                var result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, roleName);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
