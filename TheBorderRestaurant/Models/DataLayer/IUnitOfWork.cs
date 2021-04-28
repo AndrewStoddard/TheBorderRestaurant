50c9e346cbe31cb3ca3eb960f4400cffd12d6972
AndrewStoddard <acs8182000@gmail.com>
2021-04-27 20:34:56 -0400

    Added unit of work files

---
+++ TheBorderRestaurant/Models/DataLayer/IUnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿// ***********************************************************************
+// Author           : Andrew Stoddard
+// Created          : 04-27-2021
+//
+// Last Modified By : Andrew Stoddard
+// Last Modified On : 04-27-2021
+// ***********************************************************************
+
+using TheBorderRestaurant.Models.DomainModels;
+
+namespace TheBorderRestaurant.Models.DataLayer
+{
+    /// <summary>
+    ///     Interface IUnitOfWork
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets the food orders.
+        /// </summary>
+        /// <value>The food orders.</value>
+        IRepository<FoodOrder> FoodOrders { get; }
+
+        /// <summary>
+        ///     Gets the food order items.
+        /// </summary>
+        /// <value>The food order items.</value>
+        IRepository<FoodOrderItem> FoodOrderItems { get; }
+
+        /// <summary>
+        ///     Gets the food items.
+        /// </summary>
+        /// <value>The food items.</value>
+        IRepository<FoodItem> FoodItems { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Saves this instance.
+        /// </summary>
+        void Save();
+
+        #endregion
+    }
+}
---
+++ TheBorderRestaurant/Models/DataLayer/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using TheBorderRestaurant.Models.DomainModels;
+
+namespace TheBorderRestaurant.Models.DataLayer
+{
+    public class UnitOfWork
+    {
+        #region Data members
+
+        private readonly BorderContext context;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets the food orders.
+        /// </summary>
+        /// <value>The food orders.</value>
+        private IRepository<FoodOrder> FoodOrders { get; }
+
+        /// <summary>
+        ///     Gets the food order items.
+        /// </summary>
+        /// <value>The food order items.</value>
+        private IRepository<FoodOrderItem> FoodOrderItems { get; }
+
+        /// <summary>
+        ///     Gets the food items.
+        /// </summary>
+        /// <value>The food items.</value>
+        private IRepository<FoodItem> FoodItems { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public UnitOfWork(BorderContext context)
+        {
+            this.context = context;
+            this.FoodOrderItems = new Repository<FoodOrderItem>(context);
+            this.FoodOrders = new Repository<FoodOrder>(context);
+            this.FoodItems = new Repository<FoodItem>(context);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Saves this instance.
+        /// </summary>
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        #endregion
+    }
+}
