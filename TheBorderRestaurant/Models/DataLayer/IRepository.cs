94fc8a6dcea89b53340be0070bf8e61c13fe749b
AndrewStoddard <acs8182000@gmail.com>
2021-04-27 20:34:48 -0400

    Added Repository Files

---
+++ TheBorderRestaurant/Models/DataLayer/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TheBorderRestaurant.Models.DataLayer
+{
+    /// <summary>
+    ///     Interface IRepository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T : class
+    {
+        #region Methods
+
+        /// <summary>
+        ///     Gets the specified expression.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <returns>IQueryable&lt;T&gt;.</returns>
+        IQueryable<T> Get(Expression<Func<T, bool>> expression = null);
+
+        /// <summary>
+        ///     Inserts the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        void Insert(T entity);
+
+        /// <summary>
+        ///     Updates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        void Update(T entity);
+
+        /// <summary>
+        ///     Deletes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        void Delete(T entity);
+
+        #endregion
+    }
+}
---
+++ TheBorderRestaurant/Models/DataLayer/Repository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TheBorderRestaurant.Models.DataLayer
+{
+    /// <summary>
+    ///     Class Repository.
+    ///     Implements the <see cref="IRepository{T}" />
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="IRepository{T}" />
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region Data members
+
+        /// <summary>
+        ///     The context
+        /// </summary>
+        protected BorderContext context;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Repository{T}" /> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public Repository(BorderContext context)
+        {
+            this.context = context;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Gets the specified expression.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <returns>IQueryable&lt;T&gt;.</returns>
+        public virtual IQueryable<T> Get(Expression<Func<T, bool>> expression = null)
+        {
+            return expression == null ? this.context.Set<T>() : this.context.Set<T>().Where(expression);
+        }
+
+        /// <summary>
+        ///     Inserts the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        public virtual void Insert(T entity)
+        {
+            this.context.Set<T>().Add(entity);
+        }
+
+        /// <summary>
+        ///     Updates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        public virtual void Update(T entity)
+        {
+            this.context.Set<T>().Update(entity);
+        }
+
+        /// <summary>
+        ///     Deletes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        public virtual void Delete(T entity)
+        {
+            this.context.Set<T>().Remove(entity);
+        }
+
+        #endregion
+    }
+}
