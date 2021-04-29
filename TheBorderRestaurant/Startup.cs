3b8f86ab237c24ecb9692de38e3acb15ab638963
AndrewStoddard <acs8182000@gmail.com>
2021-04-28 22:14:08 -0400

    Updated the program and startup to have the DB context and the identity info

--- TheBorderRestaurant/Startup.cs
+++ TheBorderRestaurant/Startup.cs
@@ -1,29 +1,51 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TheBorderRestaurant.Models.DataLayer;
+using TheBorderRestaurant.Models.DomainModels;
 
 namespace TheBorderRestaurant
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        #region Properties
 
         public IConfiguration Configuration { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        #endregion
+
+        #region Methods
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddMemoryCache();
+            services.AddSession();
             services.AddControllersWithViews();
+            services.AddDbContext<BorderContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("BorderContext")));
+
+            services.AddIdentity<User, IdentityRole>(options =>
+                    {
+                        options.Password.RequiredLength = 6;
+                        options.Password.RequireNonAlphanumeric = false;
+                        options.Password.RequireDigit = false;
+                    }).AddEntityFrameworkStores<BorderContext>()
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,19 +61,26 @@ namespace TheBorderRestaurant
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/");
             });
+            BorderContext.CreateAdminUser(app.ApplicationServices).Wait();
         }
+
+        #endregion
     }
 }
