01483917fc58d4a1ed619937a6de88f506b4d714
AndrewStoddard <acs8182000@gmail.com>
2021-04-29 19:06:59 -0400

    Began admin page and made 2 list pages and their view models

---
+++ TheBorderRestaurant/Models/ViewModels/FoodListViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using TheBorderRestaurant.Models.DomainModels;
+
+namespace TheBorderRestaurant.Models.ViewModels
+{
+    public class FoodListViewModel
+    {
+        #region Properties
+
+        public List<FoodItem> Items { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the size of the page.
+        /// </summary>
+        /// <value>The size of the page.</value>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the page number.
+        /// </summary>
+        /// <value>The page number.</value>
+        public int PageNumber { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total pages.
+        /// </summary>
+        /// <value>The total pages.</value>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        ///     Gets the page sizes.
+        /// </summary>
+        /// <value>The page sizes.</value>
+        public int[] PageSizes { get; } = {1, 2, 3, 4, 5};
+
+        #endregion
+    }
+}
