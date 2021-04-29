eacb7908fb32cff59b2999f32f8375e8dd3ea6b0
AndrewStoddard <acs8182000@gmail.com>
2021-04-28 22:28:58 -0400

    Added some controller methods for home

--- TheBorderRestaurant/Controllers/HomeController.cs
+++ TheBorderRestaurant/Controllers/HomeController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TheBorderRestaurant.Models.DataLayer;
 
 namespace TheBorderRestaurant.Controllers
 {
     public class HomeController : Controller
     {
+        #region Data members
+
+        private readonly IUnitOfWork unitOfWork;
+
+        #endregion
+
+        #region Constructors
+
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        #endregion
+
         #region Methods
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            ViewBag.FoodItems = this.unitOfWork.FoodItems.Get().ToList();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Specials()
         {
             return View();
         }
