e04ddfa857d9d72c10100b4db38ce4cde5a04da0
AndrewStoddard <acs8182000@gmail.com>
2021-04-28 21:37:02 -0400

    Added Login and Register view models

---
+++ TheBorderRestaurant/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBorderRestaurant.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        #endregion
+    }
+}
---
+++ TheBorderRestaurant/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBorderRestaurant.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        #region Properties
+
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name.")]
+        [StringLength(255)]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name.")]
+        [StringLength(255)]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a street.")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Please enter a city.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a state.")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter a zip.")]
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        #endregion
+    }
+}
