08a2125d7d1296bc4bfc0ee8040d3576731129b2
AndrewStoddard <acs8182000@gmail.com>
2021-05-03 18:31:06 -0400

    Added tests for remove food

---
+++ TheBorderRestaurantTests/AdminControllerTests/RemoveFoodTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheBorderRestaurant.Controllers;
+using Xunit;
+
+namespace TheBorderRestaurantTests.AdminControllerTests
+{
+    public class RemoveFoodTests
+    {
+        #region Methods
+
+        [Fact]
+        public void TestRemoveFoodReturnsRedirectToActionResult()
+        {
+            var accessor = TestLibrary.TestLibrary.SetUpHttpContextAccessor();
+            var unitOfWork = TestLibrary.TestLibrary.SetUpIUnitOfWork();
+
+            var controller = new AdminController(unitOfWork.Object, accessor.Object);
+            var result = controller.RemoveFood(1) as RedirectToActionResult;
+            Assert.IsType<RedirectToActionResult>(result);
+        }
+
+        #endregion
+    }
+}
