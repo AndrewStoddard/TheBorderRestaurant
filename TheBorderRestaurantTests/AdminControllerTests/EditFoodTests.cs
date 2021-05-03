503c0e62fc9537fb326b954f998c01a82457571c
AndrewStoddard <acs8182000@gmail.com>
2021-05-03 18:30:51 -0400

    Added tests for editfood

---
+++ TheBorderRestaurantTests/AdminControllerTests/EditFoodTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheBorderRestaurant.Controllers;
+using Xunit;
+
+namespace TheBorderRestaurantTests.AdminControllerTests
+{
+    public class EditFoodTests
+    {
+        #region Methods
+
+        [Fact]
+        public void TestEditFoodReturnsView()
+        {
+            var accessor = TestLibrary.TestLibrary.SetUpHttpContextAccessor();
+            var unitOfWork = TestLibrary.TestLibrary.SetUpIUnitOfWork();
+
+            var controller = new AdminController(unitOfWork.Object, accessor.Object);
+            var result = controller.EditFood(1) as ViewResult;
+            Assert.IsType<ViewResult>(result);
+        }
+
+        #endregion
+    }
+}
