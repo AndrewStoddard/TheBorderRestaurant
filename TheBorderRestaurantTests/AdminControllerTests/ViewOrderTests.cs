2be60c3f61fb3afaa7fd4d04233ab07c1fe27800
AndrewStoddard <acs8182000@gmail.com>
2021-05-03 18:31:15 -0400

    added tests for view order

---
+++ TheBorderRestaurantTests/AdminControllerTests/ViewOrderTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheBorderRestaurant.Controllers;
+using Xunit;
+
+namespace TheBorderRestaurantTests.AdminControllerTests
+{
+    public class ViewOrderTests
+    {
+        #region Methods
+
+        [Fact]
+        public void TestViewOrderReturnsView()
+        {
+            var accessor = TestLibrary.TestLibrary.SetUpHttpContextAccessor();
+            var unitOfWork = TestLibrary.TestLibrary.SetUpIUnitOfWork();
+
+            var controller = new AdminController(unitOfWork.Object, accessor.Object);
+            var result = controller.ViewOrder(1) as ViewResult;
+            Assert.IsType<ViewResult>(result);
+        }
+
+        #endregion
+    }
+}
